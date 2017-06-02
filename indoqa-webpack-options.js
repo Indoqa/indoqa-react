6c8319c202d8b9fe4f70a48453f4b9409a11a115
Reinhard Pötz <reinhard.poetz@indoqa.com>
2017-06-02 20:51:54 +0200

    upgrade libs; use prop-types

--- indoqa-webpack-options.js
+++ indoqa-webpack-options.js
@@ -2,6 +2,7 @@ module.exports = {
   appName: 'IndoqaReactApp',
   isReactLibrary: true,
   externals: {
+    'prop-types': 'prop-types',
     'react-redux': 'react-redux',
     'react-router': 'react-router',
     redux: 'redux',
