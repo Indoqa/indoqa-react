b4aaa10e71ecb8dc35c52fbb68bb0371c775ee88
Reinhard Pötz <reinhard.poetz@indoqa.com>
2017-10-12 00:13:53 +0200

    upgrade dependencies

--- indoqa-webpack-options.js
+++ indoqa-webpack-options.js
@@ -2,7 +2,11 @@ module.exports = {
   appName: 'IndoqaReactApp',
   isReactLibrary: true,
   externals: {
+    'indoqa-react-fela': 'indoqa-react-fela',
     'prop-types': 'prop-types',
+    react: 'react',
+    'react-dom': 'react-dom',
+    'react-fela': 'react-fela',
     'react-redux': 'react-redux',
     'react-router': 'react-router',
     redux: 'redux',
