992eee1dfdaafda8c1206ce16d99a88f6f3423f5
Reinhard Pötz <reinhard.poetz@indoqa.com>
2017-12-17 22:33:01 +0100

    introduce enableLogging store param

--- src/main/createIndoqaStore.js
+++ src/main/createIndoqaStore.js
@@ -2,28 +2,19 @@ import {applyMiddleware, compose, createStore} from 'redux'
 import {createEpicMiddleware} from 'redux-observable'
 import {createLogger} from 'redux-logger'
 
-const createIndoqaStore = ({rootReducer, rootEpic, initialState = {}}) => {
-  const isProduction = process.env.NODE_ENV === 'production'
+const createIndoqaStore = ({rootReducer, rootEpic, initialState = {}, enableLogging = false}) => {
   const epicMiddleware = createEpicMiddleware(rootEpic)
   const middlewares = [epicMiddleware]
-
-  if (isProduction || typeof window === 'undefined') {
-    return {
-      reduxStore: createStore(
-        rootReducer,
-        initialState,
-        applyMiddleware(...middlewares)
-      ),
-      epicMiddleware,
-    }
+  if (enableLogging) {
+    const logger = createLogger({
+      collapsed: true,
+    })
+    middlewares.push(logger)
   }
 
-  const logger = createLogger({
-    collapsed: true,
-  })
-  middlewares.push(logger)
-
-  const devToolsExtension = window.devToolsExtension ? window.devToolsExtension() : (f) => f
+  const devToolsExtension = typeof window !== 'undefined' && window.devToolsExtension ?
+    window.devToolsExtension() :
+    (f) => f
 
   return {
     reduxStore: createStore(
