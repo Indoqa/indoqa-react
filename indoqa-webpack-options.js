7308ca71d45fc49fdc62ad6ffa70de8cdf510fda
Reinhard Pötz <reinhard.poetz@indoqa.com>
2018-04-19 11:14:28 +0200

    cleanup dependencies

--- indoqa-webpack-options.js
+++ indoqa-webpack-options.js
@@ -2,6 +2,7 @@ module.exports = {
   appName: 'IndoqaReactApp',
   isReactLibrary: true,
   externals: {
+    'fela-preset-web': 'fela-preset-web',
     'indoqa-react-fela': 'indoqa-react-fela',
     'prop-types': 'prop-types',
     react: 'react',
@@ -9,6 +10,7 @@ module.exports = {
     'react-fela': 'react-fela',
     'react-redux': 'react-redux',
     'react-router': 'react-router',
+    'react-router-redux': 'react-router',
     redux: 'redux',
     'redux-observable': 'redux-observable',
     rxjs: 'rxjs',
