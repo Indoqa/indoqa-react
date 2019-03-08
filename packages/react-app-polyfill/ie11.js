ce3e00d8eb1f2402d4887074aed8e88e6fe223ee
Reinhard Pötz <reinhard.poetz@indoqa.com>
2019-03-08 10:38:49 +0100

    add react-app-polyfill

---
+++ packages/react-app-polyfill/ie11.js
@@ -0,0 +1,4 @@
+require('react-app-polyfill/ie11')
+require('./arrayIncludesPolyfill')
+require('./nodeListForEachPolyfill')
+require('./stringStartsWithPolyfill')
---
+++ packages/react-app-polyfill/nodeListForEachPolyfill.js
@@ -0,0 +1,4 @@
+/* eslint-disable */
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = Array.prototype.forEach;
+}
