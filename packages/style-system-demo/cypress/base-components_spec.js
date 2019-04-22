2ac417788d45496a8958966ba3054769a3b83f22
Reinhard Pötz <reinhard.poetz@indoqa.com>
2019-04-22 22:56:54 +0200

    style-system-demo: cypress

---
+++ packages/style-system-demo/cypress/base-components_spec.js
@@ -0,0 +1,7 @@
+context('Base components', function () {
+    it('Box', function () {
+        cy.visit('/');
+        expect(true).to.equal(true);
+        expect(true).to.equal(true);
+    });
+});
---
+++ packages/style-system-demo/cypress/base-components_spec.ts
@@ -0,0 +1,7 @@
+context('Base components', () => {
+  it('Box', () => {
+    cy.visit('/')
+    expect(true).to.equal(true)
+    expect(true).to.equal(true)
+  })
+})
