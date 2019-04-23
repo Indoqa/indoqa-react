d972e32c4eb6a957156e42ca927e6b310a79dc9d
Reinhard Pötz <reinhard.poetz@indoqa.com>
2019-04-23 12:57:12 +0200

    style-system-demo: cypress tests + ci integration

--- packages/style-system-demo/src/base-components/BoxSamples.tsx
+++ packages/style-system-demo/src/base-components/BoxSamples.tsx
@@ -7,29 +7,29 @@ import {sample1, sample2, sample3, sample4, sample5} from './BoxSamplesCode'
 const BoxSamples: React.FC = () => (
   <>
     <h3>Box with margin, padding and background color</h3>
-    <Box<Theme> my={2} p={2} bg="primary">Box1</Box>
-    <Box<Theme> my={2} p={2} bg="accent" fontStyle="mono" italic>Box2</Box>
+    <Box<Theme> my={2} p={2} bg="primary" dataTest="box1">Box1</Box>
+    <Box<Theme> my={2} p={2} bg="accent" fontStyle="mono" italic dataTest="box2">Box2</Box>
     <Code initialShow showToggle={false}>{sample1}</Code>
 
     <h3>Box with onClick event</h3>
-    <Box<Theme> my={2} p={2} bg="primary" htmlAttrs={{onClick: () => alert('Box3 clicked')}}>Box3</Box>
+    <Box<Theme> my={2} p={2} bg="primary" htmlAttrs={{onClick: () => alert('Box3 clicked')}} dataTest="box3">Box3</Box>
     <Code initialShow showToggle={false}>{sample2}</Code>
 
     <h3>Nested boxes</h3>
-    <Box<Theme> my={2} px={2} bg="primary">
-      <Box<Theme> bg="accent" mb={1} p={1}>innerBox1</Box>
-      <Box<Theme> bg="green" p={1}>innerBox2</Box>
+    <Box<Theme> my={2} px={2} bg="primary" dataTest="box4">
+      <Box<Theme> bg="accent" mb={1} p={1} dataTest="box5">innerBox1</Box>
+      <Box<Theme> bg="green" p={1} dataTest="box6">innerBox2</Box>
     </Box>
     <Code initialShow showToggle={false}>{sample3}</Code>
 
     <h3>Multiple styled box</h3>
-    <Box<Theme> my={2} p={2} style={[{color: 'white'}, {backgroundColor: 'green'}]}>box4</Box>
+    <Box<Theme> my={2} p={2} style={[{color: 'white'}, {backgroundColor: 'green'}]} dataTest="box7">box4</Box>
     <Code initialShow showToggle={false}>{sample4}</Code>
 
     <h3>Responsive styling</h3>
-    <Box<Theme> p={[2, 0]} bg="primary">
-      <Box<Theme> bg="accent" p={1} mb={2}>innerBox1</Box>
-      <Box<Theme> bg="green" p={1}>innerBox2</Box>
+    <Box<Theme> p={[2, 0]} bg="primary" dataTest="box8">
+      <Box<Theme> bg="accent" p={1} mb={2} dataTest="box9">innerBox1</Box>
+      <Box<Theme> bg="green" p={1} dataTest="box10">innerBox2</Box>
     </Box>
     <Code initialShow showToggle={false}>{sample5}</Code>
   </>
