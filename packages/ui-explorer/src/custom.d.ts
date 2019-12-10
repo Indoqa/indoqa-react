9af2123762a3702e50ff9db8ff61eb47d9c5c633
Reinhard Pötz <reinhard.poetz@indoqa.com>
2019-12-10 09:15:22 +0100

    ui-explorer: add missing Fela TS type definition

--- packages/ui-explorer/src/custom.d.ts
+++ packages/ui-explorer/src/custom.d.ts
@@ -2,3 +2,7 @@ declare module '*.svg' {
   const content: any
   export default content
 }
+declare module 'fela-sort-media-query-mobile-first' {
+  import {TPlugin} from 'fela'
+  export default function (): TPlugin
+}
