58a8085fe305f6286b004b808737c1624511608e
Reinhard Pötz <reinhard.poetz@indoqa.com>
2021-09-01 13:45:37 +0200

    chore: cleanup

--- packages/style-system-demo/src/index.tsx
+++ packages/style-system-demo/src/index.tsx
@@ -1,13 +1,5 @@
-import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import App from './app/App'
 
 const rootEl = document.getElementById('app')
 ReactDOM.render(<App/>, rootEl)
-
-if (module.hot) {
-  module.hot.accept('./app/App', () => {
-    const NextApp = require('./app/App.tsx').default
-    ReactDOM.render(<NextApp/>, rootEl)
-  })
-}
