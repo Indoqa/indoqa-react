c9e0891257503ce84bb19c79ef1958e670b754fc
Reinhard Pötz <reinhard.poetz@indoqa.com>
2021-09-02 09:37:25 +0200

    chore(style-system-deml): use React.StrictMode

--- packages/style-system-demo/src/index.tsx
+++ packages/style-system-demo/src/index.tsx
@@ -1,5 +1,10 @@
+import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import App from './app/App'
 
-const rootEl = document.getElementById('app')
-ReactDOM.render(<App/>, rootEl)
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('app')
+)
